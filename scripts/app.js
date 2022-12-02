46f285448ff32820ea88cbd505a3b6a993591a2f
Oscar Tandioy <tato.tandioy@gmail.com>
2022-12-02 17:02:57 -0500

    2/12/2022 5:02 pm

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,66 @@
+const stileCss = document.documentElement;
+const modePage = document.querySelector('.change-mode-container');
+const iconModePage = document.querySelector('.fa-solid');
+
+const profilePictureContainer = document.querySelector('.profile-picture-container');
+
 const mobileMenu = document.querySelector(".mobile-menu");
 const desktopMenu = document.querySelector(".desktop-menu");
 const hamburger = document.querySelector(".hamburger");
-let boolean = true;
+
+let bodyColor = 'rgb(236, 236, 236)';
+let primaryColor = 'black';
+let secondaryColor = 'rgba(0, 0, 0, 0.842)';
+
+
+let positionProfilePicture = 'fixed';
+let widthProfilePicture = '100%';
+let borderRaduisProfilePicture = '0';
+let padingProfilePicture = '10% 30%';
+
 
 hamburger.addEventListener("click", () => {
-  if (boolean) {
-    mobileMenu.classList.add("show");
-    desktopMenu.classList.remove("disguise");
-    boolean = false;
-  } else {
-    mobileMenu.classList.remove("show");
-    desktopMenu.classList.add("disguise");
-    boolean = true;
-  }
+  mobileMenu.classList.toggle("show");
+  desktopMenu.classList.toggle("disguise");
 })
+
+
+
+modePage.addEventListener('click', () => {
+  iconModePage.classList.toggle('fa-sun');
+  iconModePage.classList.toggle('fa-moon')
+  stileCss.style.setProperty("--body-color", bodyColor);
+  stileCss.style.setProperty('--primary-color',primaryColor);
+  stileCss.style.setProperty('--secondary-color',secondaryColor);
+  if (bodyColor === 'rgb(236, 236, 236)'){
+    bodyColor = 'black';
+    primaryColor ='#ffffff';
+    secondaryColor = '#adadad';
+  }else {
+    bodyColor = 'rgb(236, 236, 236)';
+    primaryColor ='black';
+    secondaryColor = 'rgba(0, 0, 0, 0.842)';
+  }
+});
+
+
+profilePictureContainer.addEventListener('click', () => {
+
+  stileCss.style.setProperty('--position-profile-picture', positionProfilePicture);
+  stileCss.style.setProperty('--width-profile-picture', widthProfilePicture);
+  stileCss.style.setProperty('--border-raduis-profile-picture', borderRaduisProfilePicture);
+  stileCss.style.setProperty('--pading-profile-picture', padingProfilePicture);
+
+
+  if(positionProfilePicture === 'fixed') {
+    positionProfilePicture = 'inline-block';
+    widthProfilePicture = '28%';
+    borderRaduisProfilePicture = '50%';
+    padingProfilePicture = '0';
+  }else {
+    positionProfilePicture = 'fixed';
+    widthProfilePicture = '100%';
+    borderRaduisProfilePicture = '0';
+    padingProfilePicture = '10% 30%';
+  }
+});
